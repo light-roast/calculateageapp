6627c9ef3c552500f41b8b4ab9f2df7028760a5c
light-roast <echeverri121@gmail.com>
2023-04-18 18:10:29 -0500

    Setting states for correct styling according to bad filling of the form

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import './App.css'
 import FormBirthDate from './components/FormBirthDate'
 
 function App() {
-  const [days, setDays] = useState(0);
-  const [months, setMonths] = useState(0);
-  const [years, setYears] = useState(0);
+  const [days, setDays] = useState('--');
+  const [months, setMonths] = useState('--');
+  const [years, setYears] = useState('--');
+  const [requiredDay, setRequiredDay] = useState(false);
+  const [requiredMonth, setRequiredMonth] = useState(false);
+  const [requiredYear, setRequiredYear] = useState(false);
+
+
   
   function calculateRealAge(year, month, day, monthArr) {
+    if (day === undefined) {
+      setRequiredDay(true);
+      return;
+    };
+    if (month === undefined) {
+      setRequiredMonth(true);
+      return;
+    };
+    if (year === undefined) {
+      setRequiredYear(true);
+      return;
+    }
     const actualDate = new Date();
     let m = month;
     let y = year;
@@ -22,13 +39,16 @@ function App() {
     }
   
     setYears(actualDate.getFullYear()-y);
-     ;
+     
   }
     
 
   return (
     <main>
-      <FormBirthDate calculate={calculateRealAge}/>
+      <FormBirthDate calculate={calculateRealAge} 
+      requiredDay={requiredDay} 
+      requiredMonth={requiredMonth} 
+      requiredYear={requiredYear}/>
       <h1>{days} days, {months} months and {years} years</h1>
     </main>
   )
--- src/components/FormBirthDate.jsx
+++ src/components/FormBirthDate.jsx
@@ -1,26 +1,51 @@
 import { useState } from "react"
 
-export default function FormBirthDate({calculate}) {
-    const [day, setDay] = useState(1);
-    const [month, setMonth] = useState(1);
-    const [year, setYear] = useState(1);
+export default function FormBirthDate({calculate, requiredDay, requiredMonth, requiredYear}) {
+    const [day, setDay] = useState(undefined);
+    const [month, setMonth] = useState(undefined);
+    const [year, setYear] = useState(undefined);
     const [validDay, setValidDay] = useState(true);
     const [validMonth, setValidMonth] = useState(true);
     const [validYear, setValidYear] = useState(true);
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
     function handleDayChange(e) {
+        setValidDay(true);
         const day = e.target.value;
         setDay(day);
+        if (day < 1 || day > 31) {
+            setValidDay(false);
+        };
+        
     };
 
     function handleMonthChange(e) {
+        setValidMonth(true);
         const month = e.target.value;
         setMonth(month);
+        if (month < 1 || month > 12) {
+            setValidMonth(false);
+        };         
     };
 
     function handleYearChange(e) {
+        setValidYear(true);        
         const year= e.target.value;
         setYear(year);
+        const actualDate = new Date();    
+        if (year < 0 || year > actualDate.getFullYear()) 
+        {
+            setValidYear(false);
+          };
+        if (month === undefined) {
+            alert('You have to first fill in the month input space');
+        }
+        if (month === 2 && ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)) {
+            daysInMonth[1] = 29; // Update days in February for leap years
+        }
+        if (day > daysInMonth[month - 1]) {
+            setValidDay(false);
+        };   
     };
     
     function handleSubmit(e) {
@@ -28,43 +53,25 @@ export default function FormBirthDate({calculate}) {
 
     }
 
-    const actualDate = new Date();
-    if (day < 1 || day > 31) {
-        setValidDay(false);
-    }; 
-    if (month < 1 || month > 12) {
-        setValidMonth(false);
-    }; 
-    if (year < 1 || year > actualDate.getFullYear()) 
-    {
-        setValidMonth(false)
-      } 
-       
-    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (month === 2 && ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)) {
-          daysInMonth[1] = 29; // Update days in February for leap years
-        }
-        if (day > daysInMonth[month - 1]) {
-          setValidMonth(false);
-        }
-      
-
-    
     return (
         <form 
         className="BDForm"
         handleSubmit={handleSubmit}>
-            <div className="labelRow">
+            <section className="labelRow">
                 <label htmlFor="day">Day</label>
                 <label htmlFor="month">Month</label>
                 <label htmlFor="year">Year</label>
-            </div>
-            <div className="inputRow">
-                <input type="number" name="day" onChange={handleDayChange} required/>
-                <input type="number" name="month" onChange={handleMonthChange}/>
-                <input type="number" name="year" onChange={handleYearChange}/>
-                
-            </div>
+            </section>
+            <section className="inputRow">
+                <input type="number" className={validDay} id={requiredDay} name="day" onChange={handleDayChange} required/>
+                <input type="number" className={validMonth} id={requiredMonth} name="month" onChange={handleMonthChange}/>
+                <input type="number" className={validYear} id={requiredYear} name="year" onChange={handleYearChange}/>
+            </section>
+            <section>
+                {validDay ? (<div></div>) : (<div>Must be a valid day</div>)}
+                {validMonth ? (<div></div>) : (<div>Must be a valid month</div>)}
+                {validYear ? (<div></div>) : (<div>Must be in the past</div>)}
+            </section>
         </form>
-    )
+    );
 };
